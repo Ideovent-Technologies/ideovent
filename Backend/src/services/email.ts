04f5d7947488a565d7446934521a940e01399351
MehdiAlam <mehdialam2002@gmail.com>
2025-09-07 15:20:01 +0530

    Full project with frontend & backend

--- Backend/src/services/email.ts
+++ Backend/src/services/email.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT) || 465,
-  secure: process.env.EMAIL_SECURE === "true", // true for 465, false for 587
+  secure: process.env.EMAIL_SECURE === "true", // true for 465, false for 587 r
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
